479d32e497fe52733642b381bfc1cafe007ba2be
khanhmuy <maikhanhhuy0608@outlook.com>
2022-01-17 14:49:06 +0700

    fuck this whole intro thing man

--- script.js
+++
@@ -1,8 +0,0 @@
-function intro() {
-    var introElement = document.getElementById("intro-container");
-    var mainElement = document.getElementById("main-container");
-    setTimeout(function() {
-        introElement.style.display = "none";
-        mainElement.style.display = "block";
-    }, 4000);
-}
