6201427d567a996d1112ac0255230d8e8a12a690
khanhmuy <maikhanhhuy0608@outlook.com>
2023-05-03 08:38:32 +0700

    i should stop doxxing myself lol

--- script.js
+++ script.js
@@ -102,10 +102,3 @@ async function fetchLanyard() {
         }
     })
 }
-
-function showDropdown() {
-    document.getElementById('dropdownContent').style.display = "block";
-}
-function hideDropdown() {
-    document.getElementById('dropdownContent').style.display = "none";
-}
