cce35d896e425c4b8cfa2591b3c83edd8b64e3ad
khanhmuy <maikhanhhuy0608@outlook.com>
2024-01-04 12:35:44 +0700

    remove mbcord

--- script.js
+++ script.js
@@ -66,11 +66,6 @@ async function fetchLanyard() {
         document.getElementById("workspace").innerHTML = `${workspace}`;
         document.getElementById("file").innerHTML = `${file}`;
       } else if (name === "Jellyfin") {
-        document.getElementById("jellyfin").style.display = "block";
-        document.getElementById(
-          "jellyfin"
-        ).innerHTML = `<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-current h-[1.1em] inline align-middle"><path d="M12 .002C8.826.002-1.398 18.537.16 21.666c1.56 3.129 22.14 3.094 23.682 0C25.384 18.573 15.177 0 12 0zm7.76 18.949c-1.008 2.028-14.493 2.05-15.514 0C3.224 16.9 9.92 4.755 12.003 4.755c2.081 0 8.77 12.166 7.759 14.196zM12 9.198c-1.054 0-4.446 6.15-3.93 7.189.518 1.04 7.348 1.027 7.86 0 .511-1.027-2.874-7.19-3.93-7.19z"/></svg> ${details} | ${state}`;
-      } else if (name === "jellyfin-rpc") {
         document.getElementById('jellyfin').style.display = "block";
         document.getElementById(
           "jellyfin"
