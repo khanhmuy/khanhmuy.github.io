5974a11742ec39702f66e9761c6c29173cb85930
khanhmuy <maikhanhhuy0608@outlook.com>
2025-12-13 08:30:42 +0700

    ayayaya

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ async function fetchLanyard() {
 
   try {
     for (const activity of data.activities) {
-      const { name, state, emoji, details, assets } = activity;
+      const { name, state, emoji, details } = activity;
 
       if (name === "Custom Status") {
         document.getElementById("customStatus").style.display = "block";
@@ -85,7 +85,9 @@ async function fetchLanyard() {
         ).innerHTML = `<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-current h-[1.1em] inline align-middle"><path d="M12 0C5.376 0 0 5.376 0 12s5.376 12 12 12 12-5.376 12-12S18.624 0 12 0zm0 19.104c-3.924 0-7.104-3.18-7.104-7.104S8.076 4.896 12 4.896s7.104 3.18 7.104 7.104-3.18 7.104-7.104 7.104zm0-13.332c-3.432 0-6.228 2.796-6.228 6.228S8.568 18.228 12 18.228s6.228-2.796 6.228-6.228S15.432 5.772 12 5.772zM9.684 15.54V8.46L15.816 12l-6.132 3.54z"/></svg> ${details}`;
       }
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 
   const colors = [
     "border-surface0-dark", 
